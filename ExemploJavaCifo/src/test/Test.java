c477a7ea50105cec4fd520cd512486b7b1ae27a8
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-04-20 13:05:10 +0200

    first commmit to this project: exemple github

---
+++ ExemploJavaCifo/src/test/Test.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello java class, java is nice, isn't it?");
+
+		//int number = 55;
+		String hello = "Hello world!!!";
+		boolean isThisClassNice = true;
+		double iva = 21.00;
+
+		System.out.println("Tell me your name");
+		Scanner reader = new Scanner(System.in);
+		String name = reader.next();
+		
+		
+		
+		System.out.println(hello + ", " + name);
+		System.out.println("Tell me a number");
+		int number = reader.nextInt();
+		
+		if (number != 0) {
+			System.out.println("The number is not equal to 0");
+		}
+
+		if (number >= 1000) {
+			System.out.println("The number is at least 1000");
+		}
+
+		System.out.println("Thank you very much to colaborate wiht us .. see you soon ..");
+	}
+
+}
